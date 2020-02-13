a1d06287921a0ea073ad684ac8311fcbf2034023
Siberfox <greyfoxjob@gmail.com>
2020-02-13 15:48:09 +0600

    fix js in modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 const button = document.querySelector('#button');
 const modal = document.querySelector('#modal');
-const close = document.querySelector('#close');
+const close = document.querySelectorAll('.modal__close');
 
 button.addEventListener('click', () => {
   modal.classList.add('modal_active');
 });
-close.addEventListener('click', () => {
-  modal.classList.remove('modal_active');
+
+close.forEach(el => {
+  el.addEventListener('click', () => {
+    modal.classList.remove('modal_active');
+  });
 });
