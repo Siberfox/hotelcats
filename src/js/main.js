d06139ade376ba337e162dd70e190feb63ae3b99
Siberfox <greyfoxjob@gmail.com>
2020-02-19 22:24:48 +0600

    added popup block

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('#button');
 const modal = document.querySelector('#modal');
 const close = document.querySelectorAll('.modal__close');
+const popup = document.querySelector('#popup');
+const popupBtn = document.querySelectorAll('.popup_form');
+const popupClose = document.querySelectorAll('.popup_close');
 
 button.addEventListener('click', () => {
   modal.classList.add('modal_active');
@@ -9,5 +12,17 @@ button.addEventListener('click', () => {
 close.forEach(el => {
   el.addEventListener('click', () => {
     modal.classList.remove('modal_active');
+  });
+});
+
+popupBtn.forEach(el => {
+  el.addEventListener('click', () => {
+    popup.classList.add('popup_active');
+  });
+});
+
+popupClose.forEach(el => {
+  el.addEventListener('click', () => {
+    popup.classList.remove('popup_active');
   });
 });
