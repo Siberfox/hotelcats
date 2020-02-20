ff6143f08c4a5b20708a3bdfa0a27ad23be6641e
Siberfox <greyfoxjob@gmail.com>
2020-02-20 20:37:07 +0600

    popup success and js

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,26 @@
-const button = document.querySelector('#button');
 const modal = document.querySelector('#modal');
-const close = document.querySelectorAll('.modal__close');
+const modalButton = document.querySelector('#button');
+const modalClose = document.querySelectorAll('.modal__close');
 const popup = document.querySelector('#popup');
-const popupBtn = document.querySelectorAll('.popup_form');
-const popupClose = document.querySelectorAll('.popup_close');
+const popupButton = document.querySelectorAll('.popup_form');
+const popupClose = document.querySelectorAll('.popup--close');
 const popupForm = document.querySelector('.popup-form');
 const sendForm = document.querySelector('.popup__button');
+const popupSuccess = document.querySelector('.popup-success');
 
-button.addEventListener('click', () => {
+modalButton.addEventListener('click', () => {
   modal.classList.add('modal_active');
   document.body.style.overflow = 'hidden';
 });
 
-close.forEach(el => {
+modalClose.forEach(el => {
   el.addEventListener('click', () => {
     modal.classList.remove('modal_active');
     document.body.style.overflow = 'auto';
   });
 });
 
-popupBtn.forEach(el => {
+popupButton.forEach(el => {
   el.addEventListener('click', () => {
     popup.classList.add('popup_active');
     document.body.style.overflow = 'hidden';
@@ -29,10 +30,13 @@ popupBtn.forEach(el => {
 popupClose.forEach(el => {
   el.addEventListener('click', () => {
     popup.classList.remove('popup_active');
+    popupForm.classList.remove('popup-form_close');
+    popupSuccess.classList.remove('popup-success--open');
     document.body.style.overflow = 'auto';
   });
 });
 
 sendForm.addEventListener('click', () => {
   popupForm.classList.add('popup-form_close');
+  popupSuccess.classList.add('popup-success--open');
 });
